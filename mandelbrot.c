08446ef938092228a202aa623e25f2c1fe23f234
Ben Chen <yubangbenchen@berkeley.edu>
2019-12-04 23:37:24 -0800

    hw9

--- mandelbrot.c
+++ mandelbrot.c
@@ -15,22 +15,25 @@
 uint32_t *iterations(struct parameters params, __m256d p_real, __m256d p_imag) {
     __m256d real = _mm256_setzero_pd(), imag = _mm256_setzero_pd();
     __m256d th = _mm256_set1_pd(params.threshold * params.threshold);
-    __m256d curr;
+    __m256d curr, real_prev;
+    int i;
     uint32_t *res = malloc(4 * sizeof(uint32_t));
-    res = {0, 0, 0, 0}; 
-    for (int i = 1; i <= params.maxiters; i++) {
-        __m256d real_prev = real;
+    for (i = 0; i < 4; i++) {
+    	*(res + i) = 0;
+    }
+    for (i = 1; i <= params.maxiters; i++) {
+        real_prev = real;
         real = _mm256_sub_pd(_mm256_add_pd(_mm256_mul_pd(real, real), p_real), _mm256_mul_pd(imag, imag));
         imag = _mm256_add_pd(_mm256_mul_pd(_mm256_mul_pd(real_prev, imag), _mm256_set1_pd(2)), p_imag);
         curr = _mm256_add_pd(_mm256_mul_pd(real, real), _mm256_mul_pd(imag, imag));
-        double *comp = malloc(4 * sizeof(double));
-        _mm256_storeu_pd (comp, _mm256_cmp_pd(curr, th, 13));
-        for (int i = 0; i < 4; i++) {
-            if (*(comp + i) == 0) {
-                *(res + i) = 1;
+	double *comp = malloc(4 * sizeof(double));
+        _mm256_storeu_pd(comp, _mm256_cmp_pd(curr, th, 13));
+        for (int j = 0; j < 4; j++) {
+            if (*(comp + j) != 0) {
+                *(res + j) = 1;
             }
         }
-        free(comp);
+	free(comp);
     }
     return res;
 }
@@ -48,13 +51,13 @@ uint32_t iteration(struct parameters params, double complex point) {
 
 void mandelbrot(struct parameters params, double scale, int32_t *num_pixels_in_set) {
     int32_t num_zero_pixels = 0;
+    __m256d p_real, p_imag;
     #pragma omp parallel for
     for (int i = params.resolution; i >= -params.resolution; i--) {
         for (int j = -params.resolution; j <= params.resolution - 4; j = j + 4) {
-            double real_arr[4] = {creal(params.center) + j * scale / params.resolution, creal(params.center) + (j + 1) * scale / params.resolution, creal(params.center) + (j + 2) * scale / params.resolution, creal(params.center) + (j + 3) * scale / params.resolution};
-            double complex imag_arr[4] = {cimag(params.center) + i * scale / params.resolution * I, cimag(params.center) + (i + 1) * scale / params.resolution * I, cimag(params.center) + (i + 2) * scale / params.resolution * I, cimag(params.center) + (i + 3) * scale / params.resolution * I};
-            __m256d p_real = _mm256_loadu_pd((__m256d *) real_arr);
-            __m256d p_imag = _mm256_loadu_pd((__m256d *) imag_arr);
+            p_real = _mm256_add_pd(_mm256_set1_pd(creal(params.center)),
+                        _mm256_set_pd(j*scale/params.resolution, (j+1)*scale/params.resolution, (j+2)*scale/params.resolution, (j+3)*scale/params.resolution));
+            p_imag = _mm256_add_pd(_mm256_set1_pd(cimag(params.center)), _mm256_set1_pd(i * scale / params.resolution));
             uint32_t *res = iterations(params, p_real, p_imag);
             #pragma omp critical
             for (int i = 0; i < 4; i++) {
@@ -62,7 +65,7 @@ void mandelbrot(struct parameters params, double scale, int32_t *num_pixels_in_s
                     num_zero_pixels++;
                 }
             }
-            free(res);
+	    free(res);
         }
         for (int j = -params.resolution + 4 * ((2 * params.resolution) / 4); j <= params.resolution; j++) {
             double complex point = (params.center +
